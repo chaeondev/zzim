42bc5c6d5867dfca7746e33e4c7930ac67627d41
Chaewon Yoon <ycw0363@gmail.com>
2023-09-08 05:07:59 +0900

    feat: Network APIService Request 메서드 구현

---
+++ ShoppingSearchProject/ShoppingSearchProject/Network/APIService.swift
@@ -0,0 +1,50 @@
+//
+//  APIService.swift
+//  ShoppingSearchProject
+//
+//  Created by Chaewon on 2023/09/08.
+//
+
+import Foundation
+import Alamofire
+
+class APIService {
+    static let shared = APIService()
+
+    private let key = APIKey.naver
+    
+    private init() { }
+    
+    func searchProduct(query: String, start: Int, sort: Sort, completion: @escaping (Shopping) -> Void ) {
+        
+        // MARK: encoding 체크, 영어일때 분기처리?+대소문자, guard문 예외처리
+        guard let text = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
+        guard let url = URL(string: "https://openapi.naver.com/v1/search/shop.json") else { return }
+        
+        let header: HTTPHeaders = [
+            "X-Naver-Client-Id": "ivZGa3DZkmJrM4pamD81",
+            "X-Naver-Client-Secret": key
+        ]
+        
+        let parameters: Parameters = [
+            "query": query,
+            "display": 30,
+            "start": start,
+            "sort": sort.rawValue
+        ]
+    
+        // MARK: 통신 시간이 너무 길때 에러처리, validate -> api status code 확인
+        AF.request(url, method: .get, parameters: parameters, headers: header).validate().responseDecodable(of: Shopping.self) { response in
+            switch response.result {
+            case .success(let value):
+                completion(value)
+            case .failure(let error): // MARK: 에러 예외처리
+                print(error)
+            }
+        }
+    }
+}
+
+enum Sort: String {
+    case sim, date, asc, dsc
+}
---
+++ ShoppingSearchProject/ShoppingSearchProject/Network/Shopping.swift
@@ -0,0 +1,31 @@
+//
+//  Shopping.swift
+//  ShoppingSearchProject
+//
+//  Created by Chaewon on 2023/09/08.
+//
+
+import Foundation
+
+// MARK: - Shopping
+struct Shopping: Codable {
+    let lastBuildDate: String
+    let total, start, display: Int
+    let items: [Item]
+}
+
+// MARK: - Item
+struct Item: Codable {
+    let title: String
+    let link: String
+    let image: String
+    let lprice, hprice, mallName, productID: String
+    let productType, brand, maker: String
+
+    enum CodingKeys: String, CodingKey {
+        case title, link, image, lprice, hprice, mallName
+        case productID = "productId"
+        case productType, brand, maker
+    }
+}
+
